056767a810ad09b651ef2180a07e62f0e86291d9
Fakhri Azhar <fakhri.azzhar@gmail.com>
2025-03-25 17:16:52 +0700

    Add tools to be used for the chatbot

--- frontend/services/chat.py
+++ frontend/services/chat.py
@@ -1,26 +1,63 @@
 from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
 
 from langgraph.graph import StateGraph, START, END
 
 from models import State
 from const import LLM_MODEL
+from .chat_tools import BasicToolNode
+
+
+
+def route_tools(state: State):
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No message found in input state to tool_edge: {state}")
+
+    # Condition to route agent
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "tools"
+    return END
+
 
 
 class ChatService:
     def __init__(self):
         self.llm = ChatOpenAI(model=LLM_MODEL)
         self._graph_builder = StateGraph(State)
+        self._tool_tavily = TavilySearchResults(max_results=2)
 
     def _chatbot(self, state: State):
-        return {"messages": [self.llm.invoke(state["messages"])]}
+        llm_with_tools = self.llm.bind_tools(self._tools)
+        return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+    def _chat_tools(self):
+        self._tools = [self._tool_tavily]
+        self._tool_node = BasicToolNode(tools=self._tools)
 
     def initializing_graph(self):
+        # NOTE: Initialize tools
+        self._chat_tools()
+
+        # NOTE: build nodes
         self._graph_builder.add_node("chatbot", self._chatbot)
+        self._graph_builder.add_node("tools", self._tool_node)
+
+        # NOTE: Define the conditdional edges
+        self._graph_builder.add_conditional_edges(
+            "chatbot",
+            route_tools,
+            {"tools": "tools", END: END},
+        )
+
+        # NOTE: Define the edges
+        self._graph_builder.add_edge("tools", "chatbot")
         self._graph_builder.add_edge(START, "chatbot")
-        self._graph_builder.add_edge("chatbot", END)
 
         self.graph = self._graph_builder.compile()
-        # return graph
 
     def stream_graph(self, messages):
         for event in self.graph.stream({"messages": messages}):
