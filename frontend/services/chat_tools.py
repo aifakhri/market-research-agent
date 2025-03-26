ebc8e3bfcb2af7c6270464962747e8cc978bdb79
Fakhri Azhar <fakhri.azzhar@gmail.com>
2025-03-26 14:07:21 +0700

    add human-in the loop as tools

--- frontend/services/chat_tools.py
+++ frontend/services/chat_tools.py
@@ -1,7 +1,11 @@
 import json
 
 from langchain_core.messages import ToolMessage
+from langchain_core.tools import InjectedToolCallId, tool
 
+from langgraph.types import Command, interrupt
+
+from typing import Annotated
 
 
 class BasicToolNode:
@@ -27,3 +31,35 @@ class BasicToolNode:
                 )
             )
         return {"messages": outputs}
+
+
+
+@tool
+def human_assistance(name: str,
+                     birthday: str,
+                     tool_call_id: Annotated[str, InjectedToolCallId]) -> str:
+    """Requests assistance from a human"""
+
+    human_response = interrupt(
+        {
+            "question": "Is this correct?",
+            "name": name,
+            "birthday": birthday
+        }
+    )
+    if human_response.get("correct", "").lower().startswith("y"):
+        verified_name = name
+        verified_birthday = birthday
+        response = "Correct"
+    else:
+        verified_name = human_response.get("name", name)
+        verified_birthday = human_response.get("birthday", birthday)
+        response = f"Made a correction: {human_response}"
+
+    state_update = {
+        "name": verified_name,
+        "birthday": verified_birthday,
+        "messages": [ToolMessage(response, tool_call_id=tool_call_id)]
+    }
+
+    return Command(update=state_update)
