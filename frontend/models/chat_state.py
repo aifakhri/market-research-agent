f1ac21b1642cc4be1362646bcd7966e1c9c80e65
Fakhri Azhar <fakhri.azzhar@gmail.com>
2025-03-26 17:29:53 +0700

    Change the State class to use Sequence and BaseMessage as the annotated type. Also, add new state: Grade

--- frontend/models/chat_state.py
+++ frontend/models/chat_state.py
@@ -1,10 +1,15 @@
-from typing import Annotated
+from pydantic import BaseModel, Field
+from typing import Annotated, Sequence
 from typing_extensions import TypedDict
+
+
+from langchain_core.messages import BaseMessage
 
 from langgraph.graph.message import add_messages
 
 
 class State(TypedDict):
-    messages: Annotated[list, add_messages]
-    name: str
-    birthday: str
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+
+class Grade(BaseModel):
+    binary_score: str=Field(description="Relevance score 'yes' or 'no'")
