28e2d8aab7f5eca93bbbfc24dc2fa57788d9580d
Fakhri Azhar <fakhri.azzhar@gmail.com>
2025-03-27 17:09:29 +0700

    Finalize the _nodes.py' module

--- frontend/services/agentic_agent/_nodes.py
+++ frontend/services/agentic_agent/_nodes.py
@@ -1,7 +1,11 @@
+from langchain import hub
 from langchain_core.messages import HumanMessage
+from langchain_core.output_parsers import StrOutputParser
+
+from langsmith import traceable
 
 from models import State, llm
-from _tools import retriever
+from ._tools import retriever
 
 
 tools = [retriever]
@@ -30,6 +34,38 @@ def rewrite(state: State):
     question = messages[0].content
 
     human_msg = [
-        HumanMessage
+        HumanMessage(
+            content=f""" \n 
+                Look at the input and try to reason about the underlying semantic intent / meaning. \n 
+                Here is the initial question:
+                \n ------- \n
+                {question} 
+                \n ------- \n
+                Formulate an improved question: """,
+        )
     ]
-    
+
+    model = llm()
+    response = model.invoke(human_msg)
+    return {"messages": [response]}
+
+@traceable
+def generate(state: State):
+    """Generate answers
+    """
+    print("---GENERATE---")
+
+    messages = state["messages"]
+    question = messages[0].content
+    last_message = messages[-1]
+
+    docs = last_message.content
+
+    prompt = hub.pull("rlm/rag-prompt")
+
+    model = llm()
+
+    rag_chain = prompt | model | StrOutputParser()
+
+    response = rag_chain.invoke({"context": docs, "question": question})
+    return {"messages": [response]}
