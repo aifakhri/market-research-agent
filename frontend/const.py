379b320c34faff1e3c51e4457770f725a9a3f811
aifakhri <fakhri.azzhar@gmail.com>
2025-04-19 21:16:43 +0700

    Load environment variables

--- frontend/const.py
+++ frontend/const.py
@@ -1,1 +1,15 @@
-LLM_MODEL = "gpt-3.5-turbo"
+import os
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+
+# OPEN ROUTER
+OPEN_ROUTER_MODEL = os.environ.get("OPEN_ROUTER_MODEL")
+OPEN_ROUTER_API_KEY = os.environ.get("OPEN_ROUTER_API_KEY")
+OPEN_ROUTER_URL = os.environ.get("OPEN_ROUTER_URL")
+
+# OPENAI
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+OPENAI_EMBEDDING = os.environ.get("OPENAI_EMBEDDING")
