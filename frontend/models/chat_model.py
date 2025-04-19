5d3e6804f523f4096fb2a6b0a29282e7dadda596
aifakhri <fakhri.azzhar@gmail.com>
2025-04-19 21:16:15 +0700

    Allow ChatOpenAI object to use OpenRouter

--- frontend/models/chat_model.py
+++ frontend/models/chat_model.py
@@ -1,6 +1,12 @@
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
-from const import LLM_MODEL
+from const import (
+    OPEN_ROUTER_API_KEY,
+    OPEN_ROUTER_URL,
+    OPEN_ROUTER_MODEL,
+    OPENAI_EMBEDDING
+)
+
 
 
 def llm():
@@ -8,11 +14,15 @@ def llm():
     TODO: Add another chat model
     """
 
-    return ChatOpenAI(model=LLM_MODEL)
+    return ChatOpenAI(
+        openai_api_key=OPEN_ROUTER_API_KEY,
+        openai_api_base=OPEN_ROUTER_URL,
+        model_name=OPEN_ROUTER_MODEL,
+    )
 
 def embeddings():
     """Calling embeddings from model provider
     TODO: Add another embedding from another chat model
     """
 
-    return OpenAIEmbeddings()
+    return OpenAIEmbeddings(model=OPENAI_EMBEDDING)
