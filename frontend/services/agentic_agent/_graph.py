70e40f0eaa5814edbebc0d05608bf2867c1c6d9c
aifakhri <fakhri.azzhar@gmail.com>
2025-04-27 21:39:21 +0700

    Separating graph with the chat service

--- frontend/services/agentic_agent/_graph.py
+++ frontend/services/agentic_agent/_graph.py
@@ -1,59 +1,40 @@
 from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
 
-from models import State
-from ._nodes import rewrite, generate, agent
-from ._edges import grade_documents
-from ._tools import retriever
+
+from models import AgentState
+from ._nodes import (
+    AgentNode,
+    RetrieveNode,
+    RewriteNode,
+    GenerateNode,
+    GradeDocsNode
+) 
 
 
+state = AgentState()
 
-class Chatbot:
-    def __init__(self):
-        pass
 
-    def build_worfklow(self):
-        self.workflow = StateGraph(State)
+class AgenticGraph:
+    def __init__(self, retriever_tools):
+        self.agent_node = AgentNode()
+        self.retriever_node = RetrieveNode(retriever_tools)
+        self.grade_node = GradeDocsNode()
+        self.rewrite_node = RewriteNode()
+        self.generate_node = GenerateNode()
 
-        # define tools
-        retriever_ = ToolNode([retriever])
+    def build(self):
+        graph = StateGraph(AgentState)
 
-        # Define nodes
-        self.workflow.add_node("agent", agent)
-        self.workflow.add_node("retrieve", retriever_)
-        self.workflow.add_node("rewrite", rewrite)
-        self.workflow.add_node("generate", generate)
+        # Add nodes
+        graph.add_node("agent", self.agent_node.run)
+        graph.add_node("retrieve", self.retrieve_node.run)
+        graph.add_node("grade", self.grade_node.run)
+        graph.add_node("rewrite", self.rewrite_node.run)
+        graph.add_node("generate", self.generate_node.run)
 
-        # Define edges
-        self.workflow.add_edge(START, "agent")
-        self.workflow.add_conditional_edges(
-            "agent",
-            tools_condition,
-            {
-                "tools": "retrieve",
-                END: END,
-            }
-        )
+        # Add nodes
+        # NOTE: Conditional node
+        
 
-        self.workflow.add_conditional_edges(
-            "retrieve",
-            grade_documents
-        )
-    
-        self.workflow.add_edge("generate", END)
-        self.workflow.add_edge("rewrite", "agent")
-
-        self.graph = self.workflow.compile()
-
-    def stream_graph(self, inputs):
-        for s in self.graph.stream({"messages": inputs}):
-            print(s)
-            if s.get("agent", []):
-                yield s["agent"]["messages"][-1].content
-            elif s.get("retriever", []):
-                yield s["retriever"]["messages"][-1].content
-            elif s.get("generate", []):
-                yield s["generate"]["messages"][-1]
-
-    def evaluate_graph(self, inputs: dict) -> dict:
-        pass
+        return graph.compile()
