c06d19e1c0172990ec31d5268c0dce860206bb5b
aifakhri <fakhri.azzhar@gmail.com>
2025-04-27 21:37:03 +0700

    remove all streamlit codes and use this file to test the agentic_agent packages

--- frontend/app.py
+++ frontend/app.py
@@ -1,30 +1,5 @@
 import streamlit as st
-
-from services import ChatService
-
-
-chatbot = ChatService()
-chatbot.initializing_graph()
+from tools import RetrieverToolBuilder
 
 
-# # NOTE: initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-if prompt := st.chat_input(placeholder="Enter your message"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        latest_messages = [
-            {"role": m["role"], "content": m["content"]}
-            for m in st.session_state.messages
-        ]
-
-        response = st.write_stream(chatbot.stream_graph(messages=latest_messages))
-    st.session_state.messages.append({"role": "assistant", "content": response})
+retriever = RetrieverToolBuilder().build()
