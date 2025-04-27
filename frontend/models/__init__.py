226e39db3391405bd24356dfe86c931dff37dc71
aifakhri <fakhri.azzhar@gmail.com>
2025-04-27 21:38:53 +0700

    renaming the State class to AgentState for better reading. Adding objects to the __all__ list

--- frontend/models/__init__.py
+++ frontend/models/__init__.py
@@ -1,7 +1,17 @@
-from .chat_state import State, Grade
+from .chat_state import AgentState, Grade
 from .chat_model import llm, embeddings
 from .chat_evaluators import (
     CorrectnessGrade,
     RelevanceGrade,
     GroundedGrade
 )
+
+__all__ = [
+    "llm",
+    "embeddings",
+    "AgentState",
+    "Grade",
+    "CorrectnessGrade",
+    "RelevanceGrade",
+    "GroundedGrade"
+]
--- frontend/models/chat_state.py
+++ frontend/models/chat_state.py
@@ -2,13 +2,13 @@ from pydantic import BaseModel, Field
 from typing import Annotated, Sequence
 from typing_extensions import TypedDict
 
-
 from langchain_core.messages import BaseMessage
 
 from langgraph.graph.message import add_messages
 
 
-class State(TypedDict):
+
+class AgentState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages]
 
 class Grade(BaseModel):
