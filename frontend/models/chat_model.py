add4bf2065c30f947ee352398f2e54aa535e85a2
Fakhri Azhar <fakhri.azzhar@gmail.com>
2025-04-29 09:16:31 +0700

    Removing embeddings function

--- frontend/models/chat_model.py
+++ frontend/models/chat_model.py
@@ -1,12 +1,10 @@
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 from const import (
-    OPEN_ROUTER_API_KEY,
-    OPEN_ROUTER_URL,
-    OPEN_ROUTER_MODEL,
-    OPENAI_EMBEDDING
+    OPENROUTER_API_KEY,
+    OPENROUTER_URL,
+    OPENROUTER_MODEL
 )
-
 
 
 def llm():
@@ -15,14 +13,7 @@ def llm():
     """
 
     return ChatOpenAI(
-        openai_api_key=OPEN_ROUTER_API_KEY,
-        openai_api_base=OPEN_ROUTER_URL,
-        model_name=OPEN_ROUTER_MODEL,
+        openai_api_key=OPENROUTER_API_KEY,
+        openai_api_base=OPENROUTER_URL,
+        model=OPENROUTER_MODEL,
     )
-
-def embeddings():
-    """Calling embeddings from model provider
-    TODO: Add another embedding from another chat model
-    """
-
-    return OpenAIEmbeddings(model=OPENAI_EMBEDDING)
