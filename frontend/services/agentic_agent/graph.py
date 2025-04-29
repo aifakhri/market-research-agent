b644ff7a3ff0b308a235cebcc66034fb7e24976d
Fakhri Azhar <fakhri.azzhar@gmail.com>
2025-04-29 09:20:57 +0700

    Change graph code. Now graph only handle graph creation

--- frontend/services/agentic_agent/graph.py
+++ frontend/services/agentic_agent/graph.py
@@ -1,41 +1,55 @@
 from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
 
-
 from models import AgentState, llm
 from tools import load_tools
+from ._edges import GradeEdge
 from ._nodes import (
     AgentNode,
     RewriteNode,
     GenerateNode,
-    GradeDocsNode
 ) 
 
 
 class AgenticGraph:
-
-
     def __init__(self):
-        self.tools = load_tools()
         self.llm = llm()
-        self.llm_with_tools = self.llm.bind_tools(self.tools) 
-        self.agent_node = AgentNode()
-        self.grade_node = GradeDocsNode()
-        self.rewrite_node = RewriteNode()
-        self.generate_node = GenerateNode()
+        self.tools = load_tools()
+        self.agent_node = AgentNode(llm=self.llm, tools=self.tools)
+        self.retrieve_node = ToolNode(self.tools)
+        self.rewrite_node = RewriteNode(llm=self.llm)
+        self.generate_node = GenerateNode(llm=self.llm)
+        self.grade_edge = GradeEdge(llm=self.llm)
 
     def build(self):
-        graph = StateGraph(AgentState)
+        workflow = StateGraph(AgentState)
 
         # Add nodes
-        graph.add_node("agent", self.agent_node.run)
-        graph.add_node("retrieve", self.retrieve_node.run)
-        graph.add_node("grade", self.grade_node.run)
-        graph.add_node("rewrite", self.rewrite_node.run)
-        graph.add_node("generate", self.generate_node.run)
+        workflow.add_node("agent", self.agent_node.run)
+        workflow.add_node("retrieve", self.retrieve_node)
+        workflow.add_node("rewrite", self.rewrite_node.run)
+        workflow.add_node("generate", self.generate_node.run)
 
         # Add nodes
         # NOTE: Conditional node
-        
+        workflow.add_edge(START, "agent")
+        workflow.add_conditional_edges(
+            "agent",
+            tools_condition,
+            {
+                "tools": "retrieve",
+                END: END
+            }
+        ) 
 
-        return graph.compile()
+        workflow.add_conditional_edges(
+            "retrieve",
+            self.grade_edge.run
+        )
+
+        workflow.add_edge("generate", END)
+        workflow.add_edge("rewrite", "agent")
+
+        compiled_graph = workflow.compile()
+
+        return compiled_graph
