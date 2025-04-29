50c30be1d9c06caa3e816509d2f9b843f6022c0b
Fakhri Azhar <fakhri.azzhar@gmail.com>
2025-04-29 09:09:12 +0700

    Adding new constant for langfuse

--- frontend/const.py
+++ frontend/const.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # OPEN ROUTER
-OPEN_ROUTER_MODEL = os.environ.get("OPEN_ROUTER_MODEL")
-OPEN_ROUTER_API_KEY = os.environ.get("OPEN_ROUTER_API_KEY")
-OPEN_ROUTER_URL = os.environ.get("OPEN_ROUTER_URL")
+OPENROUTER_MODEL = os.environ.get("OPENROUTER_MODEL")
+OPENROUTER_API_KEY = os.environ.get("OPENROUTER_API_KEY")
+OPENROUTER_URL = os.environ.get("OPENROUTER_URL")
 
 # OPENAI
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
@@ -17,3 +17,9 @@ OPENAI_EMBEDDING = os.environ.get("OPENAI_EMBEDDING")
 # QDRANT
 QDRANT_ADDRESS = os.environ.get("QDRANT_ADDRESS")
 QDRANT_COLLECTION = os.environ.get("QDRANT_COLLECTION")
+QDRANT_API_KEY = os.environ.get("QDRANT_API_KEY")
+
+# LANGFUSE
+LANGFUSE_URL = os.environ.get("LANGFUSE_URL")
+LANGFUSE_SECRET_KEY = os.environ.get("LANGFUSE_SECRET_KEY")
+LANGFUSE_PUBLIC_KEY = os.environ.get("LANGFUSE_PUBLIC_KEY")
