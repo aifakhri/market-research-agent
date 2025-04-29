b16d9e66ee512e3f78308de6377953b9c59b7828
Fakhri Azhar <fakhri.azzhar@gmail.com>
2025-04-29 09:18:49 +0700

    Overhail the edge code from function-based edge to class-based edge

--- frontend/services/agentic_agent/_edges.py
+++ frontend/services/agentic_agent/_edges.py
@@ -1,41 +1,48 @@
 from typing import Literal
 from langchain_core.prompts import PromptTemplate
 
-from models import State, Grade, llm
+from langfuse.decorators import observe
+
+from models import AgentState, GradeState
 
 
-def grade_documents(state: State) -> Literal["generate", "rewrite"]:
-    """Determines whether the retrieved documents are relevant to the question
-    """
 
-    model = llm()
+class GradeEdge:
+    def __init__(self, llm):
+        self.llm = llm
 
-    llm_with_tool = model.with_structured_output(Grade)
+    @observe()
+    def run(self, state: AgentState):
+        """
+        """
 
-    prompt = PromptTemplate(
-        template="""You are a grader assessing relevance of a retrieved document to a user question. \n 
-        Here is the retrieved document: \n\n {context} \n\n
-        Here is the user question: {question} \n
-        If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. \n
-        Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question.""",
-        input_variables=["context", "question"],        
-    )
+        llm_with_tool = self.llm.with_structured_output(GradeState)
+    
+        prompt = PromptTemplate(
+            template="""You are a grader assessing relevance of a retrieved document to a user question. \n 
+                Here is the retrieved document: \n\n {context} \n\n
+                Here is the user question: {question} \n
+                If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. \n
+                Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question.""",
+            input_variables=["context", "question"],        
+        )
 
-    chain = prompt | llm_with_tool
+        chain = prompt | llm_with_tool
 
-    messages = state["messages"]
-    last_message = messages[-1]
+        messages = state["messages"]
+        last_message = messages[-1]
 
-    question = messages[0].content
-    docs = last_message.content
+        question = messages[0].content
+        docs = last_message.content
 
-    scored_result = chain.invoke({"question": question, "context": docs})
+        scored_result = chain.invoke({"question": question, "context": docs})
+    
+        score = scored_result.binary_score
 
-    score = scored_result.binary_score
-
-    if score == "yes":
-        print("---DECISION: DOCS RELEVANT---")
-        return "generate"
-    else:
-        print("---DECISION: DOCS NOT RELEVANT---")
-        return "rewrite"
+        if score == "yes":
+            print("---DECISION: DOCS RELEVANT---")
+            return "generate"
+        else:
+            print("---DECISION: DOCS NOT RELEVANT---")
+            print(score)
+            return "rewrite"
